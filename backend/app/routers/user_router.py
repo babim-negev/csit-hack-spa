629748e77d6e1e79c159d7d9646fefea997e0334
LogXx <awesome149712@gmail.com>
2024-02-25 14:36:08 +0400

    gug

--- backend/app/routers/user_router.py
+++ backend/app/routers/user_router.py
@@ -6,7 +6,7 @@ class UserLogin(BaseModel):
     password: str
 
 class UserRegister(BaseModel):
-    fio: str
+    full_name: str
     username: str
     password: str
 
@@ -15,10 +15,10 @@ router = APIRouter(tags=['user'])
 @router.post("/user/login")
 def user_login(usr_log: UserLogin):
     # TODO: токен должны записать на фронте в Cookie
-    return { 'full_name': 'Скуфф Петрович', 'role': 'student' }
+    return { 'full_name': 'Чекушкин Скуфф Петрович', 'role': 'student', "message": "Вы авторизовались!" }
 
 @router.post("/user/register")
 def user_register(usr_reg: UserRegister, response: Response):
     # TODO: работа с бд, регистрация
     response.status_code = 201
-    return
+    return { 'message': 'Пользователь успешно зарегистрирован!' }
