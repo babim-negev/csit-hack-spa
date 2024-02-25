68ac3ed57defdf9c1f275b887a2bd512bc5a4a94
LogXx <awesome149712@gmail.com>
2024-02-25 14:26:24 +0400

    dummy resp

--- backend/app/routers/user_router.py
+++ backend/app/routers/user_router.py
@@ -1,7 +1,5 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Response, status
 from pydantic import BaseModel
-
-import os
 
 class UserLogin(BaseModel):
     username: str
@@ -17,9 +15,10 @@ router = APIRouter(tags=['user'])
 @router.post("/user/login")
 def user_login(usr_log: UserLogin):
     # TODO: токен должны записать на фронте в Cookie
-    return "abobus"
+    return { 'full_name': 'Скуфф Петрович', 'role': 'student' }
 
 @router.post("/user/register")
-def user_register(usr_reg: UserRegister):
+def user_register(usr_reg: UserRegister, response: Response):
     # TODO: работа с бд, регистрация
-    return "amogus"
+    response.status_code = 201
+    return
